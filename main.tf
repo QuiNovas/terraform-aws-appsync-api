50d4a0edd525b115837d78929cb00e57dfd947df
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-11-20 19:53:48 +0530

    ignore multiple auth

--- main.tf
+++ main.tf
@@ -19,91 +19,30 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "appsync_api" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppSyncPushToCloudWatchLogs"
-  role       = "${aws_iam_role.appsync_api.name}"
+  role       = aws_iam_role.appsync_api.name
 }
 
-resource "aws_appsync_graphql_api" "appsync_api_key_auth" {
-  count               = var.authentication_type == "API_KEY" ? 1 : 0
-  authentication_type = "API_KEY"
-  name                = var.name
-
-  log_config {
-    cloudwatch_logs_role_arn = aws_iam_role.appsync_api.arn
-    field_log_level          = var.field_log_level
-  }
-
-}
-
-resource "aws_appsync_graphql_api" "appsync_cognito_auth" {
-  count               = var.authentication_type == "AMAZON_COGNITO_USER_POOLS" ? 1 : 0
-  authentication_type = "AMAZON_COGNITO_USER_POOLS"
-  name                = var.name
-
-  user_pool_config {
-    aws_region     = var.userpool_aws_region
-    default_action = var.default_action
-    user_pool_id   = var.userpool_id
-  }
-
-  log_config {
-    cloudwatch_logs_role_arn = aws_iam_role.appsync_api.arn
-    field_log_level          = var.field_log_level
-  }
-}
-
-resource "aws_appsync_graphql_api" "appsync_iam_auth" {
-  count               = var.authentication_type == "AWS_IAM" ? 1 : 0
-  authentication_type = "AWS_IAM"
-  name                = var.name
-
-  log_config {
-    cloudwatch_logs_role_arn = aws_iam_role.appsync_api.arn
-    field_log_level          = var.field_log_level
-  }
-}
-
-resource "aws_appsync_graphql_api" "appsync_openid_connect_auth" {
-  count               = var.authentication_type == "OPENID_CONNECT" ? 1 : 0
-  authentication_type = "OPENID_CONNECT"
-  name                = var.name
-
-  openid_connect_config {
-    issuer    = var.issuer
-    auth_ttl  = var.auth_ttl == "" ? null : var.auth_ttl
-    client_id = var.client_id == "" ? null : var.client_id
-    iat_ttl   = var.iat_ttl == "" ? null : var.iat_ttl
-  }
-
-
-  log_config {
-    cloudwatch_logs_role_arn = aws_iam_role.appsync_api.arn
-    field_log_level          = var.field_log_level
-  }
-}
-
-resource "aws_appsync_graphql_api" "appsync_mutiple_auth" {
-  count               = var.multiple_auth ? 1 : 0
+resource "aws_appsync_graphql_api" "appsync_api" {
   authentication_type = var.authentication_type
   name                = var.name
 
-  dynamic "additional_authentication_provider" {
-    for_each = var.additional_authentication_provider
+  dynamic "openid_connect_config" {
+    for_each = var.authentication_type == "OPENID_CONNECT" ? ["1"] : []
     content {
-      authentication_type = additional_authentication_provider.value.authentication_type
+      issuer    = var.issuer
+      auth_ttl  = var.auth_ttl == "" ? null : var.auth_ttl
+      client_id = var.client_id == "" ? null : var.client_id
+      iat_ttl   = var.iat_ttl == "" ? null : var.iat_ttl
+    }
+  }
 
-      openid_connect_config {
-        issuer    = additional_authentication_provider.value.openid_connect_config.issuer
-        auth_ttl  = additional_authentication_provider.value.openid_connect_config.auth_ttl
-        client_id = additional_authentication_provider.value.openid_connect_config.client_id
-        iat_ttl   = additional_authentication_provider.value.openid_connect_config.iat_ttl
-      }
-
-      user_pool_config {
-        #default_action      = additional_authentication_provider.value.user_pool_config.default_action
-        user_pool_id        = additional_authentication_provider.value.user_pool_config.user_pool_id
-        app_id_client_regex = additional_authentication_provider.value.user_pool_config.app_id_client_regex
-        aws_region          = additional_authentication_provider.value.user_pool_config.aws_region
-      }
+  dynamic "user_pool_config" {
+    for_each = var.authentication_type == "AMAZON_COGNITO_USER_POOLS" ? ["1"] : []
+    content {
+      app_id_client_regex = var.app_id_client_regex == "" ? null : var.app_id_client_regex
+      aws_region          = var.userpool_aws_region == "" ? null : var.userpool_aws_region
+      default_action      = var.default_action
+      user_pool_id        = var.userpool_id
     }
   }
 
--- variables.tf
+++ variables.tf
@@ -1,26 +1,42 @@
-variable "additional_authentication_provider" {
-  default     = []
-  description = "One or more additional authentication providers for the GraphqlApi"
-  type = list(object({
-    authentication_type = string
-    openid_connect_config = object({
-      issuer    = string
-      auth_ttl  = string
-      client_id = string
-      iat_ttl   = string
-    })
-    user_pool_config = object({
-     # default_action      = string
-      user_pool_id        = string
-      app_id_client_regex = string
-      aws_region          = string
-    })
-  }))
+variable "auth_ttl" {
+  type        = string
+  description = "Number of milliseconds a token is valid after being authenticated."
+  default     = ""
 }
 
 variable "authentication_type" {
   type        = string
   description = "The authentication type. Valid values: API_KEY, AWS_IAM, AMAZON_COGNITO_USER_POOLS, OPENID_CONNECT"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Client identifier of the Relying party at the OpenID identity provider. This identifier is typically obtained when the Relying party is registered with the OpenID identity provider. You can specify a regular expression so the AWS AppSync can validate against multiple client identifiers at a time"
+  default     = ""
+}
+
+variable "default_action" {
+  type        = string
+  description = "The action that you want your GraphQL API to take when a request that uses Amazon Cognito User Pool authentication doesn't match the Amazon Cognito User Pool configuration. Valid: ALLOW and DENY"
+  default     = "DENY"
+}
+
+variable "field_log_level" {
+  type        = string
+  description = "Field logging level. Valid values: ALL, ERROR, NONE."
+  default     = "ERROR"
+}
+
+variable "iat_ttl" {
+  type        = string
+  description = "Number of milliseconds a token is valid after being issued to a user."
+  default     = ""
+}
+
+variable "issuer" {
+  type        = string
+  description = "Issuer for the OpenID Connect configuration. The issuer returned by discovery MUST exactly match the value of iss in the ID Token. Required if auth is OpenID Connect"
+  default     = ""
 }
 
 variable "name" {
@@ -34,56 +50,14 @@ variable "tags" {
   default     = {}
 }
 
-variable "field_log_level" {
-  type        = string
-  description = "Field logging level. Valid values: ALL, ERROR, NONE."
-  default     = "ERROR"
-}
-
-variable "userpool_id" {
-  type        = string
-  description = "The Cognito user pool ID, required if Cognito is the auth type"
-  default     = ""
-}
-
 variable "userpool_aws_region" {
   type        = string
   description = "The AWS region in which the user pool was created, required if Cognito is the auth type"
   default     = ""
 }
 
-variable "default_action" {
+variable "userpool_id" {
   type        = string
-  description = "The action that you want your GraphQL API to take when a request that uses Amazon Cognito User Pool authentication doesn't match the Amazon Cognito User Pool configuration. Valid: ALLOW and DENY"
-  default     = "DENY"
-}
-
-variable "issuer" {
-  type        = string
-  description = "Issuer for the OpenID Connect configuration. The issuer returned by discovery MUST exactly match the value of iss in the ID Token. Required if auth is OpenID Connect"
+  description = "The Cognito user pool ID, required if Cognito is the auth type"
   default     = ""
-}
-
-variable "auth_ttl" {
-  type        = string
-  description = "Number of milliseconds a token is valid after being authenticated."
-  default     = ""
-}
-
-variable "client_id" {
-  type        = string
-  description = "Client identifier of the Relying party at the OpenID identity provider. This identifier is typically obtained when the Relying party is registered with the OpenID identity provider. You can specify a regular expression so the AWS AppSync can validate against multiple client identifiers at a time"
-  default     = ""
-}
-
-variable "iat_ttl" {
-  type        = string
-  description = "Number of milliseconds a token is valid after being issued to a user."
-  default     = ""
-}
-
-variable "multiple_auth" {
-  type        = bool
-  description = "If additional auth providers are required, set this as true"
-  default     = false
 }
