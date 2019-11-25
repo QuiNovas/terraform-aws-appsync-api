7d1b9a4721ba0ab25e2a0603867a58af476f172a
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-11-25 16:08:14 +0530

    clean up and org changes

--- main.tf
+++ main.tf
@@ -27,22 +27,22 @@ resource "aws_appsync_graphql_api" "appsync_api" {
   name                = var.name
 
   dynamic "openid_connect_config" {
-    for_each = var.authentication_type == "OPENID_CONNECT" ? ["1"] : []
+    for_each = var.openid_connect_config
     content {
-      issuer    = var.issuer
-      auth_ttl  = var.auth_ttl == "" ? null : var.auth_ttl
-      client_id = var.client_id == "" ? null : var.client_id
-      iat_ttl   = var.iat_ttl == "" ? null : var.iat_ttl
+      issuer    = openid_connect_config.value.issuer
+      auth_ttl  = openid_connect_config.value.auth_ttl == "" ? null : openid_connect_config.value.auth_ttl
+      client_id = openid_connect_config.value.client_id == "" ? null : openid_connect_config.value.client_id
+      iat_ttl   = openid_connect_config.value.iat_ttl == "" ? null : openid_connect_config.value.iat_ttl
     }
   }
 
   dynamic "user_pool_config" {
-    for_each = var.authentication_type == "AMAZON_COGNITO_USER_POOLS" ? ["1"] : []
+    for_each = var.user_pool_config
     content {
-      app_id_client_regex = var.app_id_client_regex == "" ? null : var.app_id_client_regex
-      aws_region          = var.userpool_aws_region == "" ? null : var.userpool_aws_region
-      default_action      = var.default_action
-      user_pool_id        = var.userpool_id
+      app_id_client_regex = user_pool_config.value.app_id_client_regex == "" ? null : user_pool_config.value.app_id_client_regex
+      aws_region          = user_pool_config.value.aws_region == "" ? null : user_pool_config.value.aws_region
+      default_action      = user_pool_config.value.default_action == "" ? null : user_pool_config.value.default_action
+      user_pool_id        = user_pool_config.value.user_pool_id == "" ? null : user_pool_config.value.user_pool_id
     }
   }
 
--- variables.tf
+++ variables.tf
@@ -1,24 +1,6 @@
-variable "auth_ttl" {
-  type        = string
-  description = "Number of milliseconds a token is valid after being authenticated."
-  default     = ""
-}
-
 variable "authentication_type" {
   type        = string
   description = "The authentication type. Valid values: API_KEY, AWS_IAM, AMAZON_COGNITO_USER_POOLS, OPENID_CONNECT"
-}
-
-variable "client_id" {
-  type        = string
-  description = "Client identifier of the Relying party at the OpenID identity provider. This identifier is typically obtained when the Relying party is registered with the OpenID identity provider. You can specify a regular expression so the AWS AppSync can validate against multiple client identifiers at a time"
-  default     = ""
-}
-
-variable "default_action" {
-  type        = string
-  description = "The action that you want your GraphQL API to take when a request that uses Amazon Cognito User Pool authentication doesn't match the Amazon Cognito User Pool configuration. Valid: ALLOW and DENY"
-  default     = "DENY"
 }
 
 variable "field_log_level" {
@@ -27,37 +9,30 @@ variable "field_log_level" {
   default     = "ERROR"
 }
 
-variable "iat_ttl" {
-  type        = string
-  description = "Number of milliseconds a token is valid after being issued to a user."
-  default     = ""
-}
-
-variable "issuer" {
-  type        = string
-  description = "Issuer for the OpenID Connect configuration. The issuer returned by discovery MUST exactly match the value of iss in the ID Token. Required if auth is OpenID Connect"
-  default     = ""
-}
-
 variable "name" {
   type        = string
   description = "A user-supplied name for the GraphqlApi."
 }
 
-variable "tags" {
-  type        = map(string)
-  description = "Key-value pair tags to be applied to resource that are launched in the compute environment"
-  default     = {}
+
+variable "openid_connect_config" {
+  type = list(object({
+    issuer    = string
+    auth_ttl  = string
+    client_id = string
+    iat_ttl   = string
+  }))
+  description = "Nested argument containing OpenID Connect configuration"
+  default     = []
 }
 
-variable "userpool_aws_region" {
-  type        = string
-  description = "The AWS region in which the user pool was created, required if Cognito is the auth type"
-  default     = ""
-}
-
-variable "userpool_id" {
-  type        = string
-  description = "The Cognito user pool ID, required if Cognito is the auth type"
-  default     = ""
+variable "user_pool_config" {
+  type = list(object({
+    app_id_client_regex = string
+    aws_region          = string
+    default_action      = string
+    user_pool_id        = string
+  }))
+  description = "The Amazon Cognito User Pool configuration"
+  default     = []
 }
